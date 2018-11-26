2dda4c81c3e1d1fa151be69f5f075527658e0270
Rauan <Rauan@MacBook-Pro-Aan.local>
2018-11-26 13:09:46 +0600

    OLA

---
+++ WeatherApp/Controllers/CityWeatherVC.swift
@@ -0,0 +1,30 @@
+//
+//  CityWeatherVC.swift
+//  WeatherApp
+//
+//  Created by Rauan on 11/26/18.
+//  Copyright © 2018 Rauan. All rights reserved.
+//
+
+import UIKit
+
+class CityWeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
